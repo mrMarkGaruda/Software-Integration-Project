2a1b7d5089a3f7ef592e1f29ac9713fc6494d445
fluffy29 <hmajzoub12@gmail.com>
2025-05-03 19:27:24 +0200

    ci: add GitHub Actions workflows for PR and deployment (ran prettier)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,20 @@
-import js from "@eslint/js";
-import globals from "globals";
-import prettier from "eslint-config-prettier";
-import prettierPlugin from "eslint-plugin-prettier";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     plugins: { js, prettier: prettierPlugin },
-    extends: ["js/recommended", prettier],
+    extends: ['js/recommended', prettier],
     rules: {
-      "prettier/prettier": "error"
-    }
+      'prettier/prettier': 'error',
+    },
   },
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    languageOptions: { globals: globals.node }
-  }
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.node },
+  },
 ]);
