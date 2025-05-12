66bc103db50b2bb93735b81b39eabd01f84eef1d
Mark <mark101020036@gmail.com>
2025-05-12 14:20:13 +0200

    fix: tests couldn't be found due to it looking in the wrong directory

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: 'node',
-  transform: { '^.+\\.ts$': 'ts-jest' }, // ✅ this is actually correct
-  testMatch: ['**/src/**/*.test.ts'],
+  transform: { '^.+\\.ts$': 'ts-jest' },
+  testMatch: ['**/src/**/*.test.ts', '**/__tests__/**/*.test.ts'], // Updated to include both directories
 };
